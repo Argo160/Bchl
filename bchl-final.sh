0e5e2f50795eb2878e44bbb162199995f67aafda
Argo160 <116899376+Argo160@users.noreply.github.com>
2024-10-13 01:06:02 +0330

    Update bchl-final.sh

--- bchl-final.sh
+++ bchl-final.sh
@@ -15,15 +15,42 @@ CORE(){
     echo $'\e[32m Backhaul Core in 3 seconds... \e[0m' && sleep 1 && echo $'\e[32m2... \e[0m' && sleep 1 && echo $'\e[32m1... \e[0m' && sleep 1 && {
     }    
 }
-
+Iran_bc() {
+    clear
+    protocol_selection
+}
 Kharej_bc() {
     clear
     protocol_selection
 }
 protocol_selection() {
     clear
-    
+    while true; do
+        clear
+        echo "Menu:"
+        echo "1  - TCP"
+        echo "2  - WS"
+        echo "3  - WSS"
+        echo "4  - TCP Multiplexing"
+        echo "5  - WS Multiplexing"
+        echo "6  - WSS Multiplexing"
+        echo "0  - Return"
+        read -p "Enter your choice: " protocol
+        case $choice in
+            1) pp=3000
+            2) pp=8080    
+            3) pp=8443
+            4) pp=3000    
+            5) pp=8080
+            6) pp=8443    
+            0) Break;;    
+            *) echo "Invalid choice. Please enter a valid option.";;
+        esac
+    done
 }
+
+protocol=cc
+pp=0
 # Main menu
 # check root
 [[ $EUID -ne 0 ]] && echo -e "${RED}Fatal error: ${plain} Please run this script with root privilege \n " && exit 1
@@ -49,6 +76,7 @@ clear
                     1) Iran_bc;;
                     2) Kharej_bc;;
                     3) Break;;
+                    *) echo "Invalid choice. Please enter a valid option.";;
                 esac
             done;;
         3) UNINSTALL;;
